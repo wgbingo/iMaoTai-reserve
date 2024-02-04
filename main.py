e39599fc1755cbf0ab11403a551bc33e169cafad
wgbingo <42059802+wgbingo@users.noreply.github.com>
2024-02-05 07:40:04 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -75,8 +75,8 @@ for section in configs.sections():
             if not r_success:
                 s_title = '！！失败！！茅台预约'
             s_content = s_content + r_content + shopInfo + "\n"
-        # 领取小茅运和耐力值
-        process.getUserEnergyAward(mobile)
+            # 领取小茅运和耐力值
+            process.getUserEnergyAward(mobile)
     except BaseException as e:
         print(e)
         logging.error(e)
